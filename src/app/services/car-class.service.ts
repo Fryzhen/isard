5be1801fb009cf74b829320c91239a7d1ea0dd8b
npas <noepasquer0@gmail.com>
2025-04-20 19:47:46 +0200

    services

---
+++ src/app/services/car-class.service.ts
@@ -0,0 +1,14 @@
+import {environment} from '../../environments/environment';
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CarClassService {
+  baseUrl: string = environment.apiUrl + '/carclass';
+
+  getCarClass(): Promise<any> {
+    return fetch(`${this.baseUrl}/get`).then(r => r.json());
+  }
+}
+
