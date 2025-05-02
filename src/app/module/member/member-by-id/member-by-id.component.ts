d9822abdc15c769cf5d1b9afe3d9febfb7a28107
npas <noepasquer0@gmail.com>
2025-05-02 06:27:08 +0200

    start of member page

--- src/app/module/member/member-by-id/member-by-id.component.ts
+++ src/app/module/member/member-by-id/member-by-id.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Component, ComponentRef, OnInit, ViewContainerRef, viewChild, ViewChild} from '@angular/core';
+import {ActivatedRoute, ɵEmptyOutletComponent} from '@angular/router';
 import {MemberService} from '../../../services/requests/member.service';
 import {Member} from '../../../entities/Member';
 import {NgIf} from '@angular/common';
@@ -11,12 +11,16 @@ import {Router} from '@angular/router';
   templateUrl: './member-by-id.component.html',
   styleUrls: ['./member-by-id.component.scss'],
   imports: [
-    NgIf
+    NgIf,
   ],
 })
 export class MemberByIdComponent implements OnInit {
   member?: Member = undefined;
   isCharging = true;
+
+  #componentRef?: ComponentRef<null>
+  @ViewChild('centerPannel', {static: true}) public centerPannel?: ViewContainerRef;
+  public vcr = viewChild('centerPannel', {read: ViewContainerRef});
 
   constructor(
     private route: ActivatedRoute,
@@ -26,13 +30,15 @@ export class MemberByIdComponent implements OnInit {
   ) {
     route.params.subscribe(val => {
       if (val['memberId']) {
-        this.memberService.getMember(+val['memberId'])
+        this.memberService.getMember(+val['memberId'], true)
         .then(member => {
+          console.log(member);
           this.member = member
           this.titleService.setTitle('ISARD : ' + this.member?.display_name);
           this.isCharging = false;
         })
         .catch(error => {
+          this.isCharging = false;
           console.error('Error fetching member:', error);
         });
       }
