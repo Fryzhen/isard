64237f36b45ee7fb94652dd1d6ba6b94d6fc32fb
npas <noepasquer0@gmail.com>
2025-05-29 15:03:49 +0200

    Add background commponent

---
+++ src/app/components/background/background.component.ts
@@ -0,0 +1,37 @@
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {Input} from '@angular/core';
+import {OnInit} from '@angular/core';
+
+@Component({
+  selector: 'isard-background',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './background.component.html',
+  styleUrl: './background.component.scss'
+})
+export class BackgroundComponent implements OnInit {
+  @Input({required: true}) images!: string[]
+  currentIndex = 0;
+  nextIndex = 1;
+  currentImage = '';
+  nextImage = '';
+  nextOpacity = 0;
+
+  ngOnInit(): void {
+    this.currentImage = this.images[this.currentIndex];
+    this.nextImage = this.images[this.nextIndex];
+
+    setInterval(() => {
+      this.nextIndex = (this.currentIndex + 1) % this.images.length;
+      this.nextImage = this.images[this.nextIndex];
+      this.nextOpacity = 1;
+      setTimeout(() => {
+        this.currentIndex = this.nextIndex;
+        this.currentImage = this.nextImage;
+        this.nextOpacity = 0;
+      }, 750); // fade duration
+    }, 15000);
+  }
+}
--- src/app/module/home/home.component.ts
+++ src/app/module/home/home.component.ts
@@ -1,18 +1,21 @@
+// src/app/module/home/home.component.ts
 import {Component} from '@angular/core';
-import {NotificationService} from '../../services/app-services/notification.service';
+import {BackgroundComponent} from '../../components/background/background.component';
 
 @Component({
   selector: 'isard-home',
   templateUrl: './home.component.html',
+  imports: [BackgroundComponent],
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(
-    private notificationService: NotificationService,
-  ) {
-  }
+  images = [
+    './assets/background/double-ferrari.jpg',
+    './assets/background/inside-ir18.jpg',
+    './assets/background/nascar-drift.jpg',
+    './assets/background/w12-sunset.jpg',
+    './assets/background/nascar-race.jpg',
+  ];
 
-  onClick() {
-    this.notificationService.success('Welcome to ISARD!');
-  }
+  constructor() {}
 }
