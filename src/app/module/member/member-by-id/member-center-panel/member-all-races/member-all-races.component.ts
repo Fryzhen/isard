e0d7f63ddb118e77adb9bd25cbedce1fde353fb5
Noé PASQUER <noe.pasquer@believeit.fr>
2025-06-02 18:47:50 +0200

    start all races panel

--- src/app/module/member/member-by-id/member-center-panel/member-all-races/member-all-races.component.ts
+++ src/app/module/member/member-by-id/member-center-panel/member-all-races/member-all-races.component.ts
@@ -1,12 +1,18 @@
 import {Component, Input} from '@angular/core';
 import {Race} from '../../../../../services/request-services/iracing-entities';
 import {BoxComponent} from '../../../../../components/box/box.component';
+import {LoadingScreenComponent} from '../../../../../components/loading-screen/loading-screen.component';
+import {NgIf} from '@angular/common';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   standalone: true,
   selector: 'isard-member-all-races',
   imports: [
-    BoxComponent
+    BoxComponent,
+    LoadingScreenComponent,
+    NgIf,
+    TranslatePipe
   ],
   templateUrl: './member-all-races.component.html',
   styleUrl: './member-all-races.component.scss'
