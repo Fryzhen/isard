9cd08a8f3d07702be33e184272fae3f411ebd18e
Noé PASQUER <noe.pasquer@believeit.fr>
2025-06-05 18:09:07 +0200

    feat: enhance All Races section with structured data and new item picklists

--- src/app/module/member/member-by-id/member-parameter-panel/member-parameter-panel.component.ts
+++ src/app/module/member/member-by-id/member-parameter-panel/member-parameter-panel.component.ts
@@ -1,7 +1,11 @@
 import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {MemberScreenDisplay} from "../member-by-id.component";
-import {TranslatePipe} from "@ngx-translate/core";
+import {TranslatePipe, TranslateService} from "@ngx-translate/core";
 import {BoxComponent} from "../../../../components/box/box.component";
+import {
+  ItemPicklistComponent,
+  PicklistItem
+} from "../../../../components/picklist/item-picklist/item-picklist.component";
 import {EventType} from "../../../../services/iracing-entities";
 
 export interface MemberParameters {
@@ -27,7 +31,44 @@ export class MemberParameterPanelComponent {
 
   protected readonly MemberScreenDisplay = MemberScreenDisplay;
 
+  constructor(protected readonly translateService: TranslateService) {
+  }
+
   changeScreen(screen: MemberScreenDisplay): void {
     this.screen.emit(screen);
+  }
+
+  getItemsYear(): PicklistItem[] {
+    const years: PicklistItem[] = [];
+    for (let i = new Date().getFullYear(); i >= 2008; i--) {
+      years.push({
+        id: i, text: i.toString()
+      });
+    }
+    return years;
+  }
+
+  getItemsQuarter() {
+    return [{
+      id: 1, text: this.translateService.instant("Member.ParameterPanel.AllRaces.Quarter.Q1")
+    }, {
+      id: 2, text: this.translateService.instant("Member.ParameterPanel.AllRaces.Quarter.Q2")
+    }, {
+      id: 3, text: this.translateService.instant("Member.ParameterPanel.AllRaces.Quarter.Q3")
+    }, {
+      id: 4, text: this.translateService.instant("Member.ParameterPanel.AllRaces.Quarter.Q4")
+    }];
+  }
+
+  getItemsEventType() {
+    return [{
+      id: 5, text: this.translateService.instant("Member.ParameterPanel.AllRaces.Event.Race")
+    }, {
+      id: 2, text: this.translateService.instant("Member.ParameterPanel.AllRaces.Event.Practice")
+    }, {
+      id: 3, text: this.translateService.instant("Member.ParameterPanel.AllRaces.Event.Qualify")
+    }, {
+      id: 4, text: this.translateService.instant("Member.ParameterPanel.AllRaces.Event.TimeTrial")
+    }];
   }
 }
