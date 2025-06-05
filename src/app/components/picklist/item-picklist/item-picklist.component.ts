047058aa746a3565e7a381b22a351c506c860081
Noé PASQUER <noe.pasquer@believeit.fr>
2025-06-05 16:16:44 +0200

    feat: add standalone item-picklist component with basic functionality

---
+++ src/app/components/picklist/item-picklist/item-picklist.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {NgForOf} from "@angular/common";
+
+export interface PicklistItem {
+  text: string,
+  id: number,
+}
+
+@Component({
+  standalone: true,
+  selector: 'isard-item-picklist',
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './item-picklist.component.html',
+  styleUrl: './item-picklist.component.scss'
+})
+export class ItemPicklistComponent {
+  @Input() label = "";
+  @Input() items: PicklistItem[] = [];
+
+  @Output() itemClick = new EventEmitter<number>();
+}
