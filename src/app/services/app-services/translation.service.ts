6daba0a57ff6c6509794fbb92c2c2da7cc5e7938
npas <noe34380@gmail.com>
2025-06-09 17:04:34 +0200

    refactor: rename lint.yml to merge-test.yml and add test build step

--- src/app/services/app-services/translation.service.ts
+++ src/app/services/app-services/translation.service.ts
@@ -4,24 +4,31 @@ import localeEn from "@angular/common/locales/en";
 import localeFr from "@angular/common/locales/fr";
 import {TranslateService} from "@ngx-translate/core";
 
+export enum Language {
+  English = "en",
+  French = "fr"
+}
+
 @Injectable({
   providedIn: "root"
 })
 export class TranslationService {
-  selectedLanguage = "en";
+  selectedLanguage: Language = Language.English;
   private readonly translate = inject(TranslateService);
 
   public init() {
-    this.translate.addLangs(["fr", "en"]);
+    this.translate.addLangs([Language.English, Language.French]);
     this.translate.setDefaultLang(this.selectedLanguage);
-    registerLocaleData(localeFr, "fr");
-    registerLocaleData(localeEn, "en");
+    registerLocaleData(localeFr, Language.French);
+    registerLocaleData(localeEn, Language.English);
 
-    this.selectedLanguage = localStorage.getItem("selectedLanguage") ?? this.translate.getBrowserLang() ?? "en";
+    this.selectedLanguage = localStorage.getItem("selectedLanguage") as Language
+      ?? this.translate.getBrowserLang() as Language
+      ?? Language.English;
     this.setLanguage(this.selectedLanguage);
   }
 
-  public setLanguage(language: string) {
+  public setLanguage(language: Language) {
     localStorage.setItem("selectedLanguage", language);
     this.selectedLanguage = language;
     this.translate.use(language);
