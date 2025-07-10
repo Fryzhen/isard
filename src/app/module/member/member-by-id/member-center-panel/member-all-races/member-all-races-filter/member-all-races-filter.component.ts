75e99cb0055f70e2727f20b5c6ff1b39591ce3ba
npas <noe34380@gmail.com>
2025-07-10 21:38:52 +0200

    cleaning

--- src/app/module/member/member-by-id/member-center-panel/member-all-races/member-all-races-filter/member-all-races-filter.component.ts
+++ src/app/module/member/member-by-id/member-center-panel/member-all-races/member-all-races-filter/member-all-races-filter.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {Component, EventEmitter, inject, Input, OnInit, Output} from "@angular/core";
 import {SearchSeries} from "../../../../../../services/iracing-entities";
 import {
   ItemPicklistComponent,
